657a85c84aeacd95e8eca5b9271a9425aac4f719
andrew-waters <andrew@band-x-media.co.uk>
2009-10-18 20:11:07 +0000

    Updated 'update.xml' directory
    git-svn-id: file://localhost/Users/Andrew/Desktop/cvs/svn-repos/gravatar@5 a9b996df-dedd-4cce-9ffe-79192622361c

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
 
-
 Plugin::setInfos(array(
-    'id'          => 'gravatar',
-    'title'       => 'Gravatar', 
-    'description' => 'Allows you to put your Gravatar picture anywhere on your site', 
-    'version'     => '0.2',
-    'author'      => 'Andrew Waters',
-    'website'     => 'mailto:andrew@band-x.org')
+    'id'			=> 'gravatar',
+    'title'			=> 'Gravatar', 
+    'description'	=> 'Allows you to put your Gravatar picture anywhere on your site', 
+    'version'		=> '0.3',
+	'update_url'	=> 'http://www.band-x.org/update.xml',
+	'author'		=> 'Andrew Waters',
+    'website'		=> 'http://www.band-x.org')
 );
 
-
 Plugin::addController('gravatar', 'Gravatar', 'administrator,developer', TRUE);
-
 
 // Include Functions
 include('functions/gravatar.php');
