4a2ee390a6393b202d76b7ee35760bbb26a62ee7
andrew-waters <andrew@band-x-media.co.uk>
2009-10-18 19:12:43 +0000

    Fixed old frog reference
    git-svn-id: file://localhost/Users/Andrew/Desktop/cvs/svn-repos/gravatar@3 a9b996df-dedd-4cce-9ffe-79192622361c

--- functions/gravatar.php
+++ functions/gravatar.php
@@ -2,7 +2,7 @@
 
 function gravatar($email,$size) {
 
-	$default = URL_PUBLIC.'frog/plugins/gravatar/images/default.png';
+	$default = URL_PUBLIC.'wolf/plugins/gravatar/images/default.png';
 	$grav_url = "http://www.gravatar.com/avatar.php?gravatar_id=".md5($email)."&amp;default=".urlencode($default)."&amp;size=".$size;
 	echo "<img src=\"$grav_url\" style=\"float: left;\" />";
 
